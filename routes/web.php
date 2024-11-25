2fb797b32edb9f394d897b6717e847c676ae41f9
Gilberto da Silva Batista <gilberto.batista@crefaz.com.br>
2024-11-24 21:01:39 -0300

    Interpletando variaveis no html.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => '/'], function () {
+    Route::get('/', [SiteController::class, 'index']);
 });
