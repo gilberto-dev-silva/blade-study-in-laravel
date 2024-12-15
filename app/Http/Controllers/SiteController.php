35c58dd15345eb2f1f7acf641179ee055ad2f57f
Gilberto da Silva Batista <gilberto.batista@crefaz.com.br>
2024-12-14 21:28:56 -0300

    feat: creating fictitious data in the controller

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -8,10 +8,30 @@ class SiteController extends Controller
 {
     public function index()
     {
-       $data = [
-        'name' => 'Gilberto',
-        'sobre_nome' => "Silva",
-       ];
-       return view('welcome', $data);
+        $pessoas = [
+            [
+                'image' => 'https://i.pravatar.cc/150?img=' . rand(0, 50),
+                'nome' => 'Alessandro',
+                'birth' => '29/01/90',
+                'idade' => 29
+            ],
+
+            [
+                'image' => 'https://i.pravatar.cc/150?img=' . rand(0, 50),
+                'nome' => 'José',
+                'birth' => '10/11/1933',
+                'idade' => 90
+            ],
+
+            [
+                'image' => 'https://i.pravatar.cc/150?img=' . rand(0, 50),
+                'nome' => 'Maria',
+                'birth' => '01/01/2020',
+                'idade' => 2
+            ]
+        ];
+
+        $dados['pessoas'] = $pessoas;
+        return view('welcome', $dados);
     }
 }
